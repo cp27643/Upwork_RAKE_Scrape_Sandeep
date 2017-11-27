2b86450a79ec4247c591d97aa884dde788e48157
cp27643 <cphillips1993@aol.com>
2017-11-26 20:30:16 -0600

    Completed stoplist removal from ranked nltk phrases

--- rakescript.py
+++ rakescript.py
@@ -43,7 +43,7 @@ def RankWords(mytext):
         stopwords = csv.reader(stoplist)
         for word in stopwords:
             for phraseindex, phrase in enumerate(ranked_phrases):
-                if word[0] in phrase[1]:      #The need to remove word from phrase
+                if word[0] in phrase[1]:
                     """
                     Scenarios:
                     1.) the word is the only word in the phrase--> Remove the entire dict
@@ -53,31 +53,26 @@ def RankWords(mytext):
                             -word is at the end of the phrase --> delete only the word in the phrase
                     3.) the word is a subword in the phrase--> do nothing
                     """
-
+                    if word[0] == phrase[1]:
+                        del ranked_phrases[phraseindex]#Remove the entire dict Scenario 1
+                        continue
                     wordindex = str.find(phrase[1], word[0])
-                    if wordindex >0:
-                        if word[0] == phrase[1]:        #Remove the entire dict Scenario 1
-                            #Remove the entire tuple element (Scenario 1.)
-                            del ranked_phrases[phraseindex]
+                    if wordindex >=0:   #else the word isn't in the phrase
+                        if wordindex == 0 and not str.isalpha(phrase[1][wordindex+len(word[0])]): #start of the phrase and not a substring Scenario 2
+                            ranked_phrases[phraseindex][1] = str(phrase[1][wordindex+len(word[0])+1:])
                             continue
-                        if wordindex == 0:
-                            if phrase[1][wordindex+len(word[0])] == ' ':
-                                #word is in phrase, delete only the word (Scenario 2)
-                                ranked_phrases[phraseindex][1] = str(phrase[1][wordindex+len(word[0])+1:])
-                                continue
-
-                        elif phrase[1][wordindex-1] == ' ' and phrase[1][-len(word[0]):] == word[0]:#Word at end of phrase
-                            #remove the string
+                        elif phrase[1][-len(word[0]):] == word[0] and not str.isalpha(phrase[1][wordindex-1]):  #Word at end of phrase and not a substring Scenario 3
                             ranked_phrases[phraseindex][1] = str(phrase[1][:wordindex])
                             continue
-                        try:
-                            if phrase[1][wordindex-1] == ' ' and phrase[1][wordindex+len(word[0])] ==' ':
-                                ranked_phrases[phraseindex][1] == str(phrase[1][:wordindex])+str(phrase[1][wordindex+len(word[0]):])
-                                continue
-                        except IndexError:
-                            continue
+                        elif not phrase[1][-len(word[0]):] == word[0] and not str.isalpha(phrase[1][wordindex-1]) and not str.isalpha(phrase[1][wordindex+len(word[0])]):
+                            #word is in the phrase, not at the end or beginning so remove it
+                                leftphrase = phrase[1][:wordindex-1]
+                                rightphrase = phrase[1][wordindex+len(word[0]):]
+                                ranked_phrases[phraseindex][1] = str(leftphrase+rightphrase)
+    for index, phrase in enumerate(ranked_phrases):
+        if phrase[1] == '':
+            del ranked_phrases[index]
     return ranked_phrases
-
 
 def PrintOutput():
     try:
@@ -117,7 +112,7 @@ RE_SPACES = re.compile(r'\s{3,}')
 # for dirpath, dirnames, filenames in os.walk(r'C:\Users\c1phill\PycharmProjects\Rake Project\HT\www.hindustantimes.com'):
 #     pass
 webtext = []
-for root, dirs, files in os.walk(r'C:\Users\cphil\PycharmProjects\Upwork_RakeProject\HT\www.hindustantimes.com'):
+for root, dirs, files in os.walk(r'C:\Users\cphil\PycharmProjects\Upwork_RAKE_Scrape_Sandeep\HT\www.hindustantimes.com'):
     path = root.split(os.sep)
     #print((len(path) - 1) * '---', os.path.basename(root))
     for file in files:
